731b2a91115d2d51639d19f070eba60a96560888
Eliel Hojman <ehojman@gmail.com>
2016-05-20 15:53:17 +0300

    image scroller changes

--- classification/createTrainset.m
+++ classification/createTrainset.m
@@ -1,76 +1,85 @@
-%% Select points in the square to select images
-diagList = diag1;
-for i = 1:numel(diagList);
-    name = diagList{i}.name;
-    accNum = name(1:end-1);
-    side = name(end);
-    folderPath = ['sacro/dataset/',accNum];
-    volMatFile = [folderPath, '/', accNum, '.mat'];
-    borderMatFile = [folderPath,'/segBorder.mat'];
-    if ~exist(volMatFile,'file') || ~exist(borderMatFile,'file')
-        display(['Skipping ', name]);
-        continue;
-    end
+load pointsOfInterestFull.mat
+%%
+imgMmSz = [14 14];
+imgPixlSz = round(imgMmSz/0.25);
+imgs0 = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz);
+imgs0sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, 6);
+% imgs01sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, 6);
+% imgs02sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, 6);
+% imgs03sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, 6);
+disp('diag1 Points');
+imgs1 = getImageFromPoint(diag1Points,imgMmSz, imgPixlSz);
+imgs1sh = getImageFromPoint(diag1Points,imgMmSz, imgPixlSz, 6);
+% imgs12 = getImageFromPoint(diag1Points,imgMmSz, imgPixlSz, 6);
+disp('diag2 Points');
+imgs2 = getImageFromPoint(diag2Points,imgMmSz, imgPixlSz);
+imgs2sh = getImageFromPoint(diag2Points,imgMmSz, imgPixlSz,6);
+% imgs22 = getImageFromPoint(diag2Points,imgMmSz, imgPixlSz,6);
+disp('diag3 Points');
+imgs3 = getImageFromPoint(diag3Points,imgMmSz, imgPixlSz);
+imgs3sh = getImageFromPoint(diag3Points,imgMmSz, imgPixlSz,6);
 
-    load(volMatFile); % The volume is under the variable 'vol'
-    vol = dicom2niftiVol(vol,dicomInfo); % dicomInfo is also stored in the volMatFile
+%%
+imgPixlSz = [36 36];
+imgs = {imgs0; imgs0sh; imgs1; imgs1sh; imgs2; imgs2sh; imgs3; imgs3sh};
+labelsIdx = [1 1 2 2 2 2 2 2];
+totalImgs = 0;
+for i = 1:numel(imgs)
+    totalImgs = totalImgs + size(imgs{i},3);
+end
+X = zeros([imgPixlSz 1 totalImgs],'uint8');
+Y = zeros(2,totalImgs,'uint8');
 
-    load(borderMatFile); % The border segmentation is under 'segBorder', the ilium = 2 and the sacrum = 1;
-    if side == 'R'
-        seg = segBorder.R;
-    else
-        seg = segBorder.L;
-    end
-
-    sliceNum = Inf;
-    pointsOfInterest = {};
-    for j = size(vol,3):-1:1
-        sliceNum = min([sliceNum,j]);
-        if max(seg(:,:,sliceNum)) == 0
-            continue;
-        end
-        roi = seg(:,:,sliceNum);
-        roi(roi ~= 0) = 1; % Set values for ilium and sacrum to 1;
-        try
-            square = getConvhullSquare(roi);        
-        catch
-            display('Linear points in convhull');
-            continue;
-        end
-        square = square + [-50 50 -50 50]; % increase the field of view
-        % Make it same ratio
-        diffX = square(2) - square(1);
-        diffY = square(4) - square(3);
-        M = max([diffX, diffY]);
-        square = [square(1), square(1)+M, square(3), square(3)+M];
-        % Display image
-        try 
-            close all;
-            dataIn = input('(n)extSegmenation, ENTER next slice, number (slice number): ','s');
-        catch
-            display('Try again');
-            dataIn = '';
-        end        
-        imagesc(vol(:,:,sliceNum)'); title(['Slice ', num2str(sliceNum)]);
-        colormap gray(256);    
-        axis(square);
-        set(gca, 'CLim', [0, 700]);        
-        if numel(dataIn) > 0
-            if strcmp(dataIn,'n') || strcmp(dataIn,'N')
-                break;
-            end
-            if min(isstrprop(dataIn,'digit'))
-                sliceNum = str2num(dataIn);
-                continue;
-            end
-        end
-        shg;
-        disp('Select points ');
-        [x,y] = getpts();
-        for k = 1:numel(x)
-            s = struct('x',round(x),'y',round(y),'z',j,'accNum',accNum,'side',side,'diagnosis',diagList{i}.diagnosis);
-            pointsOfInterest{end+1} = s;
-        end
+j = 1;
+for i = 1:numel(imgs)    
+    myImgs = imgs{i};
+    disp(i)
+    for k = 1:size(myImgs,3)
+        %disp(k);
+        myImg = myImgs(:,:,k);
+        myImg = (myImg > 0) .* myImg;
+        X(:,:,1,j) = myImg./max(myImg(:)) .* 255;
+        Y(labelsIdx(i),j) = 1;        
+        j = j + 1;
     end
 end
 
+%% Save all images
+diagnosis = [ 0 0 1 1 2 2 3 3];
+removeall = 0;
+folderPath = ['images/allImgs'];
+mkdir(folderPath);
+for i = 1:numel(imgs);   
+    if removeall
+        rmdir(folderPath);
+        display(['Removed ' folderPath]);
+        continue;
+    end    
+    
+    myImgs = imgs{i};
+    if diagnosis(i) == 0
+        label = 'N';
+    else
+        label = 'P';
+    end
+    for j = 1:size(myImgs,3);
+        myImg = myImgs(:,:,j);
+        myImg = (myImg > 0) .* myImg;
+        myImg = myImg./max(myImg(:)) .* 255;
+        imwrite(myImg, [folderPath '/img' num2str(i) '_' num2str(j) '_' label '.jpg']);
+    end
+end
+
+%% Save train file
+trainFile = 'train.images.small.bin';
+labelsFile = 'train.labels.bin';
+fid = fopen(trainFile,'wb'); fwrite(fid,X(:),'uint8'); fclose(fid);
+%% Save labels
+fid = fopen(labelsFile,'wb'); fwrite(fid,Y(:),'uint8'); fclose(fid);
+
+% Then, create trainImages by
+%   fid = fopen(trainImages,'wb'); fwrite(fid,X(:),'uint8'); fclose(fid);
+% To create the label file, suppose we have k classes and let Y be a matrix
+% with Y(:,i) being all zeros vector except 1 in the position j, where j is
+% the correct label of example i. Then, create trainLabels by:
+%   fid = fopen(trainLabels,'wb'); fwrite(fid,Y(:),'uint8'); fclose(fid);
